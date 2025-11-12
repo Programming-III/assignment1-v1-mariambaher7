60b658084458b3e9fbe0769c4ac0dda373fa812a
mariambaher7 <mariam.baher@student.giu-uni.de>
2025-11-12 15:28:44 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -1,16 +1,17 @@
+#include <iostream>
+#include <string>
+#include"ANIMAL.h"
 #ifndef REPTILE_H
 #define REPTILE_H
-
-#include "Animal.h"
-
-//define reptile class here
-
-
-
-
-
-
-
-
-
+using namespace std;
+class reptile:public animal{
+  private:
+  bool isVenomous;
+  public :
+  reptile();
+  reptile(string name, int age,bool isHungry,bool isVenomous);
+  ~reptile();
+  
+  
+};
 #endif
